c3b31fc261902d8564bb1e5fbc1058c3508c8799
Andre <asm13august95@gmail.com>
2022-11-03 14:16:38 -0500

    initial, smart contracts, initialize frontend

---
+++ contracts/Escrow.sol
@@ -0,0 +1,103 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _id
+    ) external;
+}
+
+contract Escrow {
+    address public lender;
+    address public inspector;
+    address public nftAddress;
+    address payable public seller;
+
+    modifier onlySeller() {
+        require(msg.sender == seller, 'Only seller can do this');
+        _;
+    }
+
+    modifier onlyBuyer(uint256 _nftId) {
+        require(msg.sender == buyer[_nftId], 'Only buyer can do this');
+        _;
+    }
+
+    modifier onlyInspector() {
+        require(msg.sender == inspector, 'Only inspector can do this');
+        _;
+    }
+    
+
+    mapping (uint256 => bool) public isListed;
+    mapping (uint256 => uint256) public purchasePrice;
+    mapping (uint256 => uint256) public escrowAmount;
+    mapping (uint256 => address) public buyer;
+    mapping (uint256 => bool) public inspectionPassed;
+    mapping(uint256 => mapping(address => bool)) public approval;
+
+
+    constructor(
+        address _nftAddress, 
+        address payable _seller, 
+        address _inspector, 
+        address _lender) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
+
+    function list(uint256 _nftId,  address _buyer, uint256 _purchasePrice,  uint256 _escrowAmount) public payable onlySeller {
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftId);
+
+        isListed[_nftId] = true;
+        purchasePrice[_nftId] = _purchasePrice;
+        escrowAmount[_nftId] = _escrowAmount;
+        buyer[_nftId] = _buyer;
+    }
+
+    function depositEarnest(uint256 _nftId) public payable onlyBuyer(_nftId) {
+        require(msg.value >= escrowAmount[_nftId]);
+
+    }
+    
+    receive() external payable {}
+
+    function getBalance() public view returns(uint256){
+        return address(this).balance;
+    }
+
+    function updateInspectionStatus(uint256 _nftId, bool _passed) public onlyInspector() {
+        inspectionPassed[_nftId] = _passed;
+    }
+
+    function approveSale(uint256 _nftId) public {
+        approval[_nftId][msg.sender] = true;
+    }
+
+    function finalizeSale(uint256 _nftId) public {
+        require(inspectionPassed[_nftId]);
+        require(approval[_nftId][buyer[_nftId]]);
+        require(approval[_nftId][seller]);
+        require(approval[_nftId][lender]);
+        require(address(this).balance >= purchasePrice[_nftId]);
+
+        (bool success,) = payable(seller).call{value: address(this).balance}("");
+        require(success);
+
+        IERC721(nftAddress).transferFrom(address(this), buyer[_nftId], _nftId);
+    }
+
+    function cancelSale(uint256 _nftId) public{
+        if(inspectionPassed[_nftId] == false){
+            payable(buyer[_nftId]).transfer(address(this).balance);
+        } else {
+            payable(seller).transfer(address(this).balance);
+        }
+    }
+
+}
+
---
+++ scripts/deploy.js
@@ -0,0 +1,65 @@
+const { ethers } = require("hardhat");
+const hre = require("hardhat");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+async function main() {
+  let [buyer, seller, inspector, lender] = await ethers.getSigners();
+
+  const RealEstate = await ethers.getContractFactory("RealEstate");
+  const realEstate = await RealEstate.deploy();
+  await realEstate.deployed();
+
+  console.log(`Deplyed Real Estate contract at : ${realEstate.address}`);
+  console.log("Minting properties... \n");
+
+  for (let i = 0; i < 3; i++) {
+    const transaction = await realEstate
+      .connect(seller)
+      .mint(
+        `https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/${
+          i + 1
+        }.json`
+      );
+    await transaction.wait();
+  }
+
+  const Escrow = await ethers.getContractFactory("Escrow");
+  const escrow = await Escrow.deploy(
+    realEstate.address,
+    seller.address,
+    inspector.address,
+    lender.address
+  );
+  await escrow.deployed();
+
+  console.log(`Deployed Escrow Contract at: ${escrow.address}`);
+  console.log("Listing properties... \n");
+
+  for (let i = 0; i < 3; i++) {
+    let transaction = await realEstate
+      .connect(seller)
+      .approve(escrow.address, i + 1);
+    await transaction.wait();
+  }
+  transaction = await escrow
+    .connect(seller)
+    .list(1, buyer.address, tokens(20), tokens(10));
+
+  transaction = await escrow
+    .connect(seller)
+    .list(2, buyer.address, tokens(20), tokens(10));
+
+  transaction = await escrow
+    .connect(seller)
+    .list(3, buyer.address, tokens(20), tokens(10));
+
+  console.log("Finished");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
