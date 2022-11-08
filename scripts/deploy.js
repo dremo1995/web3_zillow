de2b1543d6c212f74df60f5fd8f8b6d7ff963725
Andre <asm13august95@gmail.com>
2022-11-08 10:34:51 -0600

    final commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -19,7 +19,7 @@ async function main() {
     const transaction = await realEstate
       .connect(seller)
       .mint(
-        `https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/${
+        `https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/${
           i + 1
         }.json`
       );
